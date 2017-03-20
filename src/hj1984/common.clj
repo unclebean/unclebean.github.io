79364c99d83f5b66d5f8ff3ada55776a692c2e1f
unclebean <unclebean2013@gmail.com>
2017-03-20 19:58:04 +0800

    add google analytic

--- src/hj1984/common.clj
+++ src/hj1984/common.clj
@@ -7,6 +7,17 @@
             [clj-time.coerce :as tc]
             [clj-time.format :as tf]))
 
+(defn google-analytic [] 
+  [:script "
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-94017226-1', 'auto');
+  ga('send', 'pageview');
+"])
+
 (defn head [global-meta]
   [:head
    [:title (:site-title global-meta)]
@@ -20,6 +31,7 @@
    (include-js "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js")
    (include-js "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/languages/clojure.min.js")
    [:script "hljs.initHighlightingOnLoad();"]
+   (google-analytic)
    ])
 
 (defn head-with-keywords [global-meta title]
